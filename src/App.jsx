ee3e0fa7643cc5e060ddb53406e978db93d641ae
Valentin Ibargüengoitia <valentinibar@hotmail.com>
2023-08-01 11:05:10 +0200

    Primer proyecto con React Vite

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Listado de alumnos</h1>
+
+    <img src= "src\img\digital.png" alt="foto" />
+    <ul>
+        <li>Maria Silva</li>
+        <li>Valentin Ibar</li>
+        <li>Gretel Bruing</li>
+        <li>Jose Pablo Medero</li>
+        <li>Juan Ignacio Araujo</li>
+        <li>Laura Toda</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
